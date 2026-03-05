5e3cdf7442f6a51d5873703628c07cdcba724f91
ShyamG1980 <shyamgokani5@gmail.com>
2026-03-05 16:48:55 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE = 'c4u-fx-quote-v1';
-
+// Force clear all caches and serve fresh
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
